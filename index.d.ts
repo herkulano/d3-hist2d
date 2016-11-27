02ec7fac05dd7f3eae00ce29fc063c8578eca6b6
Herculano Campos <im@herkulano.com>
2016-11-27 23:57:15 +0200

    fix typings

--- index.d.ts
+++ index.d.ts
@@ -1,29 +1,25 @@
-declare module 'd3' {
-  export function hist2d<T>(): Hist2D<T>;
+export interface Hist2D {
+  (data: Array<number[]>, callback: (hist: Array<[number, number]>) => void): Hist2D;
 
-  interface Hist2D<T> {
-    (data: Array<Array<number>>, callback: (hist: Array<Array<number>>) => void): Hist2D<T>;
+  // hist2d.bins(number) => b
+  bins(): number;
+  bins(n: number): Hist2D;
 
-    // hist2d.bins(number) => b
-    bins(): number;
-    bins(n: number): Hist2D<T>;
+  // hist2d.indices([x, y]) => [x, y]
+  indices(): [number, number];
+  indices(indices: [number, number]): Hist2D;
 
-    // hist2d.indices([x, y]) => [x, y]
-    indices(): number[];
-    indices(indices: number[]): Hist2D<T>;
+  // hist2d.interval(number) => t
+  interval(): number;
+  interval(value: number): Hist2D;
 
-    // hist2d.interval(number) => t
-    interval(): number;
-    interval(value: number): Hist2D<T>;
+  // hist2d.domain([[x1,x2],[y1,y2]]) => d
+  domain(): [[number, number], [number, number]];
+  domain(domain: [[number, number], [number, number]]): Hist2D;
 
-    // hist2d.domain([[x1,x2],[y1,y2]]) => d
-    domain(): Array<Array<number>>;
-    domain(domain: Array<Array<number>>): Hist2D<T>;
-
-    // hist2d.size([width, height]) => s
-    size(): number[];
-    size(values: number[]): Hist2D<T>;
-  }
+  // hist2d.size([width, height]) => s
+  size(): [number, number];
+  size(values: [number, number]): Hist2D;
 }
 
-export = 'd3';
+export function hist2d(): Hist2D;
